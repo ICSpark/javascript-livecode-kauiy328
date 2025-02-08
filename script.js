512947e0be4d6fc751a54210a2791c6a81b5f228
kauiy328 <kauiy38711@gmail.com>
2025-02-08 20:20:00 +0000

    Done

---
+++ script.js
@@ -0,0 +1,25 @@
+alert("Welcome");
+//This means to print "Hello World" also position matters if you put this at the end it will print at the end
+console.log("Hello World");
+
+var name = "ICSpark";
+
+//These are our money variables
+var money1 = 70;
+var money2 = 150;
+
+console.log(name);
+
+console.log(money1 + money2)
+
+console.log(money1);
+console.log(money2);
+
+if (money1 < 100) {
+    console.log("This person is poor");
+} 
+else {
+    console.log("This person is rich!!");
+}
+
+alert("Welcome");
